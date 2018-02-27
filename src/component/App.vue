3a1201d057b9bae5b4056a2dc7052cf9cbac9793
heminhua <15218482830@163.com>
2018-02-27 16:49:02 +0800

    环境测试

---
+++ src/component/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="red">{{val}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      val: "hello word",
+
+      
+    };
+  },
+  
+};
+</script>
+
+<style scoped>
+.red{
+    color:rgb(107, 1, 63);
+    font-size: 40px;
+}
+</style>
