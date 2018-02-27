c8b04bf92397a791c2118d699502e6ce2618fb9e
heminhua <15218482830@163.com>
2018-02-27 20:22:13 +0800

    element-ui 测试

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
         <div class="red">{{val}}</div>
+        <!-- switch 开关 -->
+        <el-switch style="display: block" v-model="payWay" active-color="#13ce66" inactive-color="#ff4949" active-text="按月" inactive-text="按年">
+        </el-switch>
+        <!-- button -->
+        <el-button type="primary" round>
+            <router-link to="./index">index</router-link>
+        </el-button>
+        <el-button type="primary" round>
+            <router-link to="./login">登录</router-link>
+        </el-button>
+        <el-button type="primary" round>
+            <router-link to="./register">注册</router-link>
+        </el-button>
+        <!-- ** -->
+        <p>我是{{counts}}</p>
+        <!-- <button @click="getLoginPage">计算</button> -->
+        <button @click="getLoginPage">计算</button>
+        <!-- 使用自定义组件 -->
         <router-view></router-view>
     </div>
 </template>
@@ -10,17 +28,32 @@ export default {
   data() {
     return {
       val: "hello word",
-
-      
+      payWay: true,
+      counts: 1,
+      isShow: true
     };
   },
-  
+  methods: {
+    getLoginPage() {
+      this.counts = this.counts + 1;
+    },
+    toggle() {
+      this.isShow = !this.isShow;
+    }
+  }
 };
 </script>
 
 <style scoped>
-.red{
-    color:rgb(107, 1, 63);
-    font-size: 40px;
+.red {
+  color: rgb(107, 1, 63);
+  font-size: 40px;
+}
+button {
+  margin-top: 10px;
+}
+a {
+  color: #fff;
+  text-decoration: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './component/App.vue'
 import router from './router/index.js'// 引用路由 /index.js可以省略
+
+//引入element-ui插件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//使用element-ui
+Vue.use(ElementUI)
+
 new Vue({
     el:'#app',  
     router,//路由注入Vue的实例中
-
+    routes:[
+        {name:'index',path:'/index',component:App}//定义主路由
+    ],
     //下面使用的箭头函数也是Es6 的语法
     render:createElement=>createElement(App)//渲染数据
 })
