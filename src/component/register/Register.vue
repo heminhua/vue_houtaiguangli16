37ca2734e23863189492da6bc49e2a9d5020bc14
heminhua <15218482830@163.com>
2018-02-28 08:35:17 +0800

    测试Element-ui

--- src/component/register/Register.vue
+++ src/component/register/Register.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <p>{{title}}</p>
-         <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-        <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-  </el-tabs>
-    </div>
+  <div>
+    <p>{{title}}</p>
+    <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
@@ -15,14 +15,14 @@ export default {
   data() {
     return {
       title: "注册页面",
-      activeName2: 'first'
+      activeName2: "first"
     };
   },
-   methods: {
-      handleClick(tab, event) {
-        console.log(tab, event);
-      }
+  methods: {
+    handleClick(tab, event) {
+      // console.log(tab, event);
     }
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 //使用element-ui
 Vue.use(ElementUI)
 
+// 3.1 导入配置后的axios与api, 并注入到Vue原型中 将来在实例通过this.$http调用
+import axios from './js/axios_config.js';
+import api from './js/api_config.js';
+Vue.prototype.$http = axios;
+Vue.prototype.$api = api;
+
 new Vue({
     el:'#app',  
     router,//路由注入Vue的实例中
@@ -16,4 +22,4 @@ new Vue({
     ],
     //下面使用的箭头函数也是Es6 的语法
     render:createElement=>createElement(App)//渲染数据
-})
+}) 
